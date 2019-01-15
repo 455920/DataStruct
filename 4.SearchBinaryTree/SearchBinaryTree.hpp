45717777225844bc0e5ceddb706d9855552a210b
455920 <1910084514@qq.com>
2019-01-15 10:52:04 +0800

    C++实现搜索二叉树的递归删除操作

--- 4.SearchBinaryTree/SearchBinaryTree.hpp
+++ 4.SearchBinaryTree/SearchBinaryTree.hpp
@@ -28,6 +28,26 @@ public:
    :pRoot_(NULL)
   {}
 
+ ~SBTree()
+ {
+   Destroy();
+   std::cout<<"析构"<<std::endl;
+ }
+
+  void Destroy()
+  {
+   Destroy_(pRoot_); 
+  }
+
+  void Destroy_(pNode cur)
+  {
+    if(cur==NULL)
+      return;
+    Destroy_(cur->left_);
+    Destroy_(cur->right_);
+    delete cur;
+  }
+
    pNode BuyNode(T value)
    {
        pNode tmp = new Node(value);
@@ -123,6 +143,8 @@ public:
       }      
     }
     
+
+    //中序遍历
     void InOrderPrint()
     {
       InOrderPrint_(pRoot_);
@@ -137,5 +159,85 @@ public:
     }
 
 
-    
+    //搜索节点
+    pNode Find(const T& value)
+    {
+      Find_(value,pRoot_);
+    }
+    pNode Find_(const T& value,pNode cur)
+    {
+      if(cur==NULL)
+        return NULL;
+      if(cur->value_==value)
+        return cur;
+      //去左子树中搜索
+     pNode tmp =  Find_(value,cur->left_);
+     if(tmp)
+       return tmp;
+      //去右子树中搜索
+     return  Find_(value,cur->right_);
+    }
+
+
+
+
+
+    bool RemoveR(const T& value)
+    {
+        return RemoveR_(value,pRoot_);
+    }
+    //1.要删除的节点是叶子节点，直接删除
+    //2.要删除的节点只有左子树，让它的左子树代替它的位置，然后删除它的左子树
+    //3.要删除的节点只有右子树，让它的左子树代替它的位置，然后删除它的右子树
+    //4.要删除的节点有左子树又有右子树，找到它的右子树的最左节点，然后与之交换值，然后递归删除改值即可
+   bool RemoveR_(const T& value,pNode& cur)//传入指针本身
+    {
+       if(cur==NULL)
+         return false;
+
+        if(value < cur->value_)
+        {
+         return  RemoveR_(value,cur->left_);
+        }
+        else if(value > cur->value_)
+        {
+          return  RemoveR_(value,cur->right_);
+        }
+        else 
+        {
+          if(cur->left_==NULL&&cur->right_==NULL)
+          {
+            pNode del = cur;
+            cur = NULL;
+            delete del;
+          }
+          //有左无
+          else if(cur->left_&&cur->right_==NULL)
+          {
+            pNode del = cur ;
+            cur=cur->left_;
+            delete del;
+          }
+          //无左有右
+          else if(cur->right_&&cur->left_==NULL) 
+          {
+            pNode del =cur ;
+            cur= cur->right_;
+            delete del;
+          }
+          else 
+          {
+            //寻找右子树的最左子树
+            pNode replace = cur->right_;
+            while(replace->left_)
+            {
+              replace = replace->left_;
+            }
+            cur->value_=replace->value_;
+            //递归删除
+            return RemoveR_(replace->value_,cur->right_);
+          }
+          return true; 
+        }
+    }   
 }; 
--- 4.SearchBinaryTree/test.cpp
+++ 4.SearchBinaryTree/test.cpp
@@ -26,9 +26,50 @@ void TestInsertR()
   tree.InsertR(0);
   tree.InOrderPrint();
 }
+
+void TestFind()
+{
+  SBTree<int> tree;
+  tree.InsertR(1);
+  tree.InsertR(10);
+  tree.InsertR(4);
+  tree.InsertR(6);
+  tree.InsertR(2);
+  tree.InsertR(8);
+  tree.InsertR(0);
+  SBTreeNode<int>* tmp = tree.Find(8);
+  if(tmp)
+  {
+    std::cout<<tmp->value_<<std::endl;
+  }
+  else 
+  {
+   std::cout<<"no find"<<std::endl;
+  }
+}
+
+
+void TestRemove() 
+{
+  SBTree<int> tree;
+  tree.InsertR(1);
+  tree.InsertR(10);
+  tree.InsertR(4);
+  tree.InsertR(6);
+  tree.InsertR(2);
+  tree.InsertR(8);
+  tree.InsertR(0);
+  tree.InOrderPrint();
+  std::cout<<"----"<<std::endl;
+  tree.RemoveR(4);
+  tree.InOrderPrint();
+}
+
 int main()
 {
- // TestInsertNR(); 
-  TestInsertR(); 
+  //TestInsertNR(); 
+  //TestInsertR(); 
+  //TestFind();
+  TestRemove();
   return 0;
 }
