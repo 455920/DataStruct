843c7cd41b94ba2d811c95642edb20c708a4223d
455920 <1910084514@qq.com>
2019-01-26 23:10:17 +0800

    堆

---
+++ 5.heap/heap.hpp
@@ -0,0 +1,32 @@
+#pragma once 
+
+
+
+template<class T>
+struct heap_node
+{
+  typedef heap_node<T> node;
+  typedef heap_node<T>* pnode;
+  T d;
+   
+};
+
+
+template<class T>
+class heap
+{
+  public:
+    bool insert(const T& d)
+    {
+      
+    }
+
+    bool remove()
+    {
+
+    }
+    
+    
+  private:
+    
+};
